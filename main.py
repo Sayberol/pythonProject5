f29a32376ca3c1b563a8204fc281cdcfdf661b38
saybe <Sayberol>
2021-12-02 14:38:58 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import json
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    with open('settings.json') as f:
+        settings = json.load(f)
+    return render_template('index.html', **settings)
+
+
+@app.route('/candidate/<id>')
+def prof(id):
+    with open('candidates.json') as f:
+        candidates = json.load(f)
+    for candidate in candidates:
+        if candidate['id'] == int(id):
+            return render_template('candidate.html', **candidate)
+
+
+@app.route('/list')
+def list_candidates():
+    with open('candidates.json') as f:
+        candidates = json.load(f)
+    return render_template('list.html', users=candidates)
+
+
+@app.route('/search/')
+def search_page():
+    name = request.args.get("name")
+    with open('candidates.json') as f:
+        candidates = json.load(f)
+    users = []
+    if name:
+        for candidate in candidates:
+            if name in candidate["name"]:
+                users.append(candidate["name"])
+    return render_template('find_name.html', users=users, count=len(users))
+
+
+@app.route('/skill/<skill>')
+def find_skill(skill):
+    with open('candidates.json') as f:
+        candidates = json.load(f)
+    with open('settings.json') as f:
+        settings = json.load(f)
+    users = []
+    count = 0
+    for candidate in candidates:
+        if skill in candidate["skills"]:
+            users.append(candidate["name"])
+            count += 1
+            if settings['limit'] == count:
+                return render_template('find_name.html', users=users, count=len(users))
+    return render_template('find_name.html', users=users, count=len(users))
+
+
+app.run()
